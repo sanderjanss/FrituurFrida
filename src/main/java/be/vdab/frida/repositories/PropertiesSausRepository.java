fe7a3eb6ace3e881309b1071f5c2cc8db708af0b
Sander Janssens <sander-ja@hotmail.com>
2019-07-10 15:58:02 +0200

    update2

--- src/main/java/be/vdab/frida/repositories/PropertiesSausRepository.java
+++ src/main/java/be/vdab/frida/repositories/PropertiesSausRepository.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 
